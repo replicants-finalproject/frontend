34806756b39448a06ddae42fcec48f9e903c0a21
carolinegbowski <caroline.gbowski@gmail.com>
2020-01-22 11:37:10 -0500

    merge conflicts resolved

--- src/container/NonProfit/NPNewRoute.js
+++ src/container/NonProfit/NPNewRoute.js
@@ -4,13 +4,8 @@ import React from 'react';
 function NPNewRoute() {
   return (
     <div>
-<<<<<<< HEAD
       <p>NP New Route Page</p>
       <label>Enter route below: </label>
-=======
-      <p>NPNewRoute Page</p>
-      {/* <label>Enter route below: </label>
->>>>>>> 7b87a82b042c06081cd30e5e9fb9cffd3ef501d9
       <input key={'starting_location'}>Enter location</input>
       <p>to</p>
       <input key={'ending_location'}>Enter Location</input>
